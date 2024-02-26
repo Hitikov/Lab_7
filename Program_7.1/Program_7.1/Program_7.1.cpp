e3e74e8f28b5ec119591e400961f5da1a9e8c96b
Hitikov <artshv131204@gmail.com>
2024-02-26 09:02:05 +0500

    Added all files

---
+++ Program_7.1/Program_7.1/Program_7.1.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string deliting(string str) {
+	bool is_odd = false;
+	int i = 0;
+	while (str[i] != '\0') {
+		if ((!is_odd) && (str[i] == 32)) {
+			is_odd = true;
+			str.erase(i, 1);
+		}
+		if ((is_odd) && (str[i] == 32)) {
+			is_odd = false;
+			++i;
+		}
+		if ((is_odd) && (str[i] != 32)) {
+			str.erase(i, 1);
+		}
+		if ((!is_odd) && (str[i] != 32)) {
+			++i;
+		}
+	}
+	return str;
+}
+
+int deliting(int* mas, int n) {
+	int is_deleted = 0;
+	for (int i = 0; i < n; ++i) {
+		if (*(mas+i) % 2 == 0) {
+			for (int j = i; j < n - 1; ++j) {
+				*(mas + j) = *(mas + j + 1);
+			}
+			++is_deleted;
+		}
+	}
+	return is_deleted;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int nav, dels;
+	string str;
+
+	int mas[100] = {};
+	int n;
+
+	cout << "Выберете тип: \n1. Строка \n2. Массив целых чисел \nВведите номер: ";
+	cin >> nav;
+	switch (nav) {
+	case 1:
+		cout << "Введите строку: ";
+		cin.ignore();
+		getline(cin, str);
+
+		str = deliting(str);
+
+		cout << "Полученная строка: " << endl;
+		cout << str;
+		break;
+	case 2:
+		cout << "Введите длину массива: ";
+		cin >> n;
+
+		cout << "Вводите элементы массива: " << endl;
+		for (int i = 0; i < n; ++i) {
+			cin >> mas[i];
+		}
+
+		dels = deliting(&mas[0], n);
+
+		cout << "Полученный массив: " << endl;
+		for (int i = 0; i < n - dels; ++i) {
+			cout << mas[i] << ' ';
+		}
+		break;
+	}
+	return 0;
+}
---
+++ Program_7.2/Program_7.2/Program_7.2.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double fun_len(double x, ...);
+double square(double x, ...);
+double squaren(int n, double x, ...);
+
+int main()
+{
+	double pos[10][2] = { 
+		{1.0, 1.0}, 
+		{4.0, 3.0}, 
+		{4.0, 1.0}, 
+		{1.0, 6.0}, 
+		{0.0, 0.0}, 
+		{0.0, 0.0}, 
+		{0.0, 0.0}, 
+		{0.0, 0.0}, 
+		{0.0, 0.0}, 
+		{0.0, 0.0}};
+
+	cout << "Square of the polygon: " << (double)squaren(4, pos[0][0], pos[0][1], pos[1][0], pos[1][1], pos[2][0], pos[2][1], pos[3][0], pos[3][1]) << endl;
+	system("pause");
+	return 0;
+}
+
+double fun_len(double x, ...)
+{
+	double len;
+	double* nav = &x;
+
+	double x1 = *(nav++);
+	double y1 = *(nav++);
+	double x2 = *(nav++);
+	double y2 = *(nav);
+
+	len = pow(pow(x1 - x2, 2) + pow(y1 - y2, 2), 0.5);
+
+	return len;
+}
+
+double square(double x, ...)
+{
+	double* nav = &x;
+	double x1, y1, sq;
+	double l1, l2, l3;
+
+	x1 = *(nav++);
+	y1 = *(nav++);
+
+	l1 = fun_len(x1, y1, *nav, *(nav + 1));
+	l2 = fun_len(x1, y1, *(nav + 2), *(nav + 3));
+	l3 = fun_len(*nav, *(nav + 1), *(nav + 2), *(nav + 3));
+
+	double p = (l1 + l2 + l3) * 0.5;
+
+	sq = pow(p * (p - l1) * (p - l2) * (p - l3), 0.5);
+
+	return sq;
+}
+
+double squaren(int n, double x, ...)
+{
+	double* nav = &x;
+	double x1, y1, sq = 0;
+
+	x1 = *(nav++);
+	y1 = *(nav++);
+
+	for (int i = 1; i <= n - 2; ++i)
+	{
+		sq += square(x1, y1, *nav, *(nav + 1), *(nav + 2), *(nav + 3));
+		nav += 2;
+	}
+
+	return sq;
+}
